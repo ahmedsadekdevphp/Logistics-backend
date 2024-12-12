78f88b3c11032cc41f71f23567dac6ae08dbaf5b
ahmed.sadek <ahmed.sadek@acmesaico.com>
2024-12-12 19:44:19 +0200

    build dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,6 @@
-<x-app-layout>
+@extends('layouts.app')
+
+@section('content')
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -6,12 +8,22 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        <div class="container">
+            <!-- Add small card for orders -->
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    <div class="card shadow-lg h-100">
+                        <div class="card-body d-flex justify-content-between align-items-center">
+                            <a href="{{ route('admin.orders.index') }}" class="text-decoration-none text-dark d-flex align-items-center">
+                                <div class="h5 mb-0"><i class="fa fa-car"></i> {{ __('Orders') }}</div>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="ml-2" width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="M10 6l6 6-6 6" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
